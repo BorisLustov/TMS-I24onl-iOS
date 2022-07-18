89b87b331480b14bb2b1144fd86bfcd892e44e78
BorisLustov <107603925+BorisLustov@users.noreply.github.com>
2022-07-18 17:40:36 +0300

    update hw

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,82 +7,185 @@
 
 import UIKit
 
-let number0 = UIView()
-let number1 = UIView()
-let number2 = UIView()
-let number3 = UIView()
-let number4 = UIView()
-let number5 = UIView()
-let number6 = UIView()
-let number7 = UIView()
-let number8 = UIView()
-let number9 = UIView()
-let dot = UIView()
-
-let AC = UIView()
-let plusOrMinus = UIView()
-let persent = UIView()
-
-let division = UIView()
-let multiplication = UIView()
-let subtraction = UIView()
-let addition = UIView()
-let equally = UIView()
+enum TypeOfButton {
+    case numbers, calculating, operating
+}
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - UIStackView
+    
+    private let allStackButton = UIStackView()
+    
+    private let firstStackButton = UIStackView()
+    private let secondStackButton = UIStackView()
+    private let thirdStackButton = UIStackView()
+    private let fourStackButton = UIStackView()
+    private let fifthStackButton = UIStackView()
+    
+    private let number0 = UIButton()
+    let number1 = UIButton()
+    let number2 = UIButton()
+    let number3 = UIButton()
+    let number4 = UIButton()
+    let number5 = UIButton()
+    let number6 = UIButton()
+    let number7 = UIButton()
+    let number8 = UIButton()
+    let number9 = UIButton()
+    let dot = UIButton()
+    
+    let AC = UIButton()
+    let plusOrMinus = UIButton()
+    let persent = UIButton()
+    
+    let division = UIButton()
+    let multiplication = UIButton()
+    let subtraction = UIButton()
+    let addition = UIButton()
+    let equally = UIButton()
+    
+    // MARK: - Struct objects
+    
+    private var variable = Variables()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black
+        
+        setupAllStackButton()
+        
+        setupStackButton(fifthStackButton)
+        setupStackButton(fourStackButton)
+        setupStackButton(thirdStackButton)
+        setupStackButton(secondStackButton)
+        setupStackButton(firstStackButton)
+        
+        setupButton(number0, "0", .numbers, firstStackButton)
+        setupButton(dot, ",", .numbers, firstStackButton)
+        setupButton(equally, "=", .calculating, firstStackButton)
+        
+        setupButton(number1, "1", .numbers, secondStackButton)
+        setupButton(number2, "2", .numbers, secondStackButton)
+        setupButton(number3, "3", .numbers, secondStackButton)
+        setupButton(addition, "+", .calculating, secondStackButton)
+        
+        setupButton(number4, "4", .numbers, thirdStackButton)
+        setupButton(number5, "5", .numbers, thirdStackButton)
+        setupButton(number6, "6", .numbers, thirdStackButton)
+        setupButton(subtraction, "-", .calculating, thirdStackButton)
+        
+        setupButton(number7, "7", .numbers, fourStackButton)
+        setupButton(number8, "8", .numbers, fourStackButton)
+        setupButton(number9, "9", .numbers, fourStackButton)
+        setupButton(multiplication, "*", .calculating, fourStackButton)
+        
+        setupButton(AC, "AC", .operating, fifthStackButton)
+        setupButton(plusOrMinus, "+/-", .operating, fifthStackButton)
+        setupButton(persent, "%", .operating, fifthStackButton)
+        setupButton(division, "/", .calculating, fifthStackButton)
+        
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    private func setupAllStackButton() {
+        allStackButton.translatesAutoresizingMaskIntoConstraints = false
+        allStackButton.axis = .vertical
+        allStackButton.alignment = .center
+        allStackButton.spacing = 10
         
-        let height = view.bounds.width / 4
-        let width = height
-        let widthZero = height * 2
+        view.addSubview(allStackButton)
         
-        viewParameters(view: number0, x: view.bounds.minX, y: view.bounds.maxY - (height * 2), width: widthZero, height: height, color: .darkGray, lableText: "0", lableColor: .white)
-        viewParameters(view: number1, x: view.bounds.minX, y: view.bounds.maxY - (height * 3), width: width, height: height, color: .darkGray, lableText: "1", lableColor: .white)
-        viewParameters(view: number2, x: view.bounds.minX + width, y: view.bounds.maxY - (height * 3), width: width, height: height, color: .darkGray, lableText: "2", lableColor: .white)
-        viewParameters(view: number3, x: view.bounds.minX + (width * 2), y: view.bounds.maxY - (height * 3), width: width, height: height, color: .darkGray, lableText: "3", lableColor: .white)
-        viewParameters(view: number4, x: view.bounds.minX, y: view.bounds.maxY - (height * 4), width: width, height: height, color: .darkGray, lableText: "4", lableColor: .white)
-        viewParameters(view: number5, x: view.bounds.minX + width, y: view.bounds.maxY - (height * 4), width: width, height: height, color: .darkGray, lableText: "5", lableColor: .white)
-        viewParameters(view: number6, x: view.bounds.minX + (width * 2), y: view.bounds.maxY - (height * 4), width: width, height: height, color: .darkGray, lableText: "6", lableColor: .white)
-        viewParameters(view: number7, x: view.bounds.minX, y: view.bounds.maxY - (height * 5), width: width, height: height, color: .darkGray, lableText: "7", lableColor: .white)
-        viewParameters(view: number8, x: view.bounds.minX + width, y: view.bounds.maxY - (height * 5), width: width, height: height, color: .darkGray, lableText: "8", lableColor: .white)
-        viewParameters(view: number9, x: view.bounds.minX + (width * 2), y: view.bounds.maxY - (height * 5), width: width, height: height, color: .darkGray, lableText: "9", lableColor: .white)
-        viewParameters(view: dot, x: view.bounds.minX + (width * 2), y: view.bounds.maxY - (height * 2), width: width, height: height, color: .darkGray, lableText: ",", lableColor: .white)
-        
-        viewParameters(view: AC, x: view.bounds.minX, y: view.bounds.maxY - (height * 6), width: width, height: height, color: .gray, lableText: "AC", lableColor: .black)
-        viewParameters(view: plusOrMinus, x: view.bounds.minX + width, y: view.bounds.maxY - (height * 6), width: width, height: height, color: .gray, lableText: "+/-", lableColor: .black)
-        viewParameters(view: persent, x: view.bounds.minX + (width * 2), y: view.bounds.maxY - (height * 6), width: width, height: height, color: .gray, lableText: "%", lableColor: .black)
-        
-        viewParameters(view: division, x: view.bounds.minX + (width * 3), y: view.bounds.maxY - (height * 6), width: width, height: height, color: .orange, lableText: "/", lableColor: .white)
-        viewParameters(view: multiplication, x: view.bounds.minX + (width * 3), y: view.bounds.maxY - (height * 5), width: width, height: height, color: .orange, lableText: "*", lableColor: .white)
-        viewParameters(view: subtraction, x: view.bounds.minX + (width * 3), y: view.bounds.maxY - (height * 4), width: width, height: height, color: .orange, lableText: "-", lableColor: .white)
-        viewParameters(view: addition, x: view.bounds.minX + (width * 3), y: view.bounds.maxY - (height * 3), width: width, height: height, color: .orange, lableText: "+", lableColor: .white)
-        viewParameters(view: equally, x: view.bounds.minX + (width * 3), y: view.bounds.maxY - (height * 2), width: width, height: height, color: .orange, lableText: "=", lableColor: .white)
+        NSLayoutConstraint.activate([
+            allStackButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30)
+        ])
     }
     
-    func viewParameters(view: UIView, x: Double, y: Double, width: Double, height: Double, color: UIColor, lableText: String, lableColor: UIColor) {
-        view.frame = CGRect(x: x, y: y, width: width, height: height)
-        view.backgroundColor = color
-        view.layer.cornerRadius = view.bounds.height / 2
-        self.view.addSubview(view)
+    private func setupStackButton(_ stack: UIStackView) {
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.alignment = .center
+        stack.spacing = 10
+        
+        allStackButton.addArrangedSubview(stack)
+    }
+    
+    private func setupButton(_ button: UIButton, _ text: String, _ typeButton: TypeOfButton, _ stack: UIStackView) {
+        
+        variable.width = view.bounds.width / 4.5
+        variable.height = variable.width
+        variable.widthZero = (variable.width * 2) + 10
+        
+        switch typeButton {
+        case .numbers:
+            button.backgroundColor = .darkGray
+        case .calculating:
+            button.backgroundColor = .orange
+        case .operating:
+            button.backgroundColor = .lightGray
+        }
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        switch button {
+        case number0:
+            NSLayoutConstraint.activate([
+                button.widthAnchor.constraint(equalToConstant: variable.widthZero),
+                button.heightAnchor.constraint(equalToConstant: variable.height)
+            ])
+            button.bounds = CGRect(x: 0, y: 0, width: variable.widthZero, height: variable.height)
+        default:
+            NSLayoutConstraint.activate([
+                button.widthAnchor.constraint(equalToConstant: variable.width),
+                button.heightAnchor.constraint(equalToConstant: variable.height)
+            ])
+            button.bounds = CGRect(x: 0, y: 0, width: variable.width, height: variable.height)
+        }
+                
+        button.layer.cornerRadius = button.bounds.height / 2
+        button.clipsToBounds = true
+        
+        switch stack {
+        case firstStackButton:
+            firstStackButton.addArrangedSubview(button)
+        case secondStackButton:
+            secondStackButton.addArrangedSubview(button)
+        case thirdStackButton:
+            thirdStackButton.addArrangedSubview(button)
+        case fourStackButton:
+            fourStackButton.addArrangedSubview(button)
+        case fifthStackButton:
+            fifthStackButton.addArrangedSubview(button)
+        default:
+            break
+        }
         
         let lable = UILabel()
-        lable.frame = view.bounds
-        lable.text = lableText
-        lable.textColor = lableColor
+        lable.frame = button.bounds
+        lable.text = text
         lable.textAlignment = .center
         lable.font = UIFont.systemFont(ofSize: 40)
-        view.addSubview(lable)
         
+        switch typeButton {
+        case .operating:
+            lable.textColor = .black
+        default:
+            lable.textColor = .white
+        }
         
+        button.addSubview(lable)
     }
+}
+
+extension ViewController {
     
+    private struct Variables {
+        
+        var height = CGFloat()
+        var width = CGFloat()
+        var widthZero = CGFloat()
+    }
 }
 
